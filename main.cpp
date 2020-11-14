a0bc7d138cb7cd0e25fd828a5046bd181f83a157
jDan735 <daniel734@bk.ru>
2020-11-14 08:20:34 +0300

    Add logo

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 void event(int type, int bot){
     string results[3] = {"Победа", "Проигрыш", "Ничья"};
-    string objects[3] = {"камень", "ножницы", "бумага"};
-    cout << " " << results[type] << "! " << "У бота был " << objects[bot] << endl;
+    string objects[3] = {" камень", "и ножницы", "а бумага"};
+    cout << " " << results[type] << "! " << "У бота был" << objects[bot] << ". " << endl;
 }
 
 void game(int user){
@@ -21,8 +21,6 @@ void game(int user){
 
     user--;
     bot--;
-
-    // cout << "user, bot = " << user << ", " << bot << endl;
 
     if (user <= 2){
         cout << "Бот выбирает..." << endl << endl;
@@ -40,9 +38,11 @@ void game(int user){
 }
 
 int main(){
-    cout << "                         " << endl;
-    cout << " Камень, ножницы, бумага " << endl;
-    cout << "                         " << endl;
+    cout << "                       _           " << endl;
+    cout << "  ___ __ _ _ __   ___ | |__  _   _ " << endl;
+    cout << " / __/ _` | '_ \\ / _ \\| '_ \\| | | |" << endl;
+    cout << "| (_| (_| | | | | (_) | |_) | |_| |" << endl;
+    cout << " \\___\\__,_|_| |_|\\___/|_.__/ \\__,_|" << endl << endl;
     cout << "1. Камень" << endl;
     cout << "2. Ножницы" << endl;
     cout << "3. Бумага" << endl;
